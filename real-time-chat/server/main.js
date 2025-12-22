fec442d8de79806ee2b89d6f251783975af22673
sujalcharati <sujalc024@gmail.com>
2025-12-22 22:03:16 +0530

    ws lib

---
+++ real-time-chat/server/main.js
@@ -0,0 +1,53 @@
+import express from "express"
+import { WebSocketServer } from "ws";
+import http from "http"
+import { join } from "path";
+
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server})
+
+wss.on("connection",(ws)=>{
+    console.log(" client connected")
+    ws.on("message",(data)=>{
+
+        try{
+
+            const {username ,message} = JSON.parse(data);
+
+            const outgoing = JSON.stringify({ username, message});
+            wss.clients.forEach( (client)=>{
+                if( client.readyState === WebSocket.OPEN){
+                    client.send(outgoing);
+                }
+            })
+
+        }catch(err){
+            console.error(" invalid messages ",err);
+
+        }
+
+    })
+
+    ws.on("close",(data)=>{
+        console.log(" client closed successfully")
+    });
+});
+
+app.get("/health",( req, res)=>{
+
+    return res.status(200).json(
+        {
+            message:" server is healthy"
+        }
+    )
+})
+
+server.listen(port,()=>{
+    console.log(` the server is runnning at port ${port} `);
+})
