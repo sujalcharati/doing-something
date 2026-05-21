bfa1960ccbae9904f2f8434db7f9b8459416ff14
sujalcharati <sujalc024@gmail.com>
2026-05-21 14:50:59 +0530

    assign-2

---
+++ summer_practise/a_to_z/components/Portfoliohero.jsx
@@ -0,0 +1,24 @@
+export const Portfoliohero = ( )=>{
+
+    return (
+        <main className="  px-5  h-screen flex gap-6 ">
+            
+            <div className="flex flex-col ml-32 mt-10 w-">
+
+            <img src="https://images.unsplash.com/photo-1587691592099-24045742c181?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className=" h-46 mt-32 "/>
+
+            <a className=" mt-44 ml-24 text-3xl " href="https://images.unsplash.com/photo-1587691592099-24045742c181?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">resume</a>
+            </div>
+
+
+             <div className=" flex-col h-screen  text-2xl pt-48 ml-52">
+            <a className=" border-b-slate-400 absolute right-20 mr-0" href="https://sujalcharati.xyz">contact me </a>
+
+            <h3 className="  mt-32 text-5xl font-mono px-10 h-20 "> hey I'm Sujal, Full stack developer </h3>
+
+            <p className="flex  pt-10 pl-10"> Im final year student who loves time wasting doomscrooling , play chess, matiks etc..  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab vitae, reiciendis animi laborum iste ex repellat cupiditate quas alias minima saepe esse quia quasi deserunt modi commodi pariatur qui. Numquam.</p>
+             </div>
+
+        </main>
+    )
+}
